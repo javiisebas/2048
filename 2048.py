6ac647bbaaa970d746271c062e64db71e5825a32
Javier Sebastián Fernández <32978953+javiisebas@users.noreply.github.com>
2020-04-17 12:00:26 +0200

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -5,10 +5,10 @@ import random as rd
 
 pygame.init()
 win = pygame.display.set_mode((440,545)) 
-pygame.display.set_caption("JAVIER SEBASTIÁN FERNÁNDEZ")
+pygame.display.set_caption("2048")
 clock = pygame.time.Clock()
 
-Numeros = [pygame.image.load('images/2.png'), pygame.image.load('images/4.png'), pygame.image.load('images/8.png'),
+Numbers_Images = [pygame.image.load('images/2.png'), pygame.image.load('images/4.png'), pygame.image.load('images/8.png'),
            pygame.image.load('images/16.png'), pygame.image.load('images/32.png'), pygame.image.load('images/64.png'),
            pygame.image.load('images/128.png'), pygame.image.load('images/256.png'), pygame.image.load('images/512.png'),
            pygame.image.load('images/1024.png'), pygame.image.load('images/2048.png'), pygame.image.load('images/4096.png')]
@@ -20,7 +20,7 @@ GameOver = pygame.image.load("images/gameOver.png")
 GameRestart = pygame.image.load("images/gameOver_Restart.png")
 
 
-class malla(object):
+class Mesh(object):
     def __init__(self):
         self.x = 20
         self.y = 125
@@ -35,256 +35,255 @@ class malla(object):
         self.x = 20
         self.y = 125
 
-mallas = malla()
+mesh = Mesh()
 
-class matriz(object):
+class Matrix(object):
     def __init__(self):
-        self.numMatriz = np.zeros([4,4],float)
-        self.tam = len(self.numMatriz)
-        self.arriba = False
-        self.abajo = False
-        self.izquierda = False
-        self.derecha = False
+        self.numMatrix = np.zeros([4,4],float)
+        self.length = len(self.numMatrix)
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
         self.moves = False
-        self.muerto = False
-        self.cont = 0
-        self.numMatrizAntes = np.empty(np.shape(self.numMatriz))
+        self.death = False
+        self.counter = 0
+        self.numMatrizBefore = np.empty(np.shape(self.numMatrix))
     
-    def aparece(self):
+    def appear(self):
         self.pos_x = int(rd.randint(0,3))
         self.pos_y = int(rd.randint(0,3))
-        while self.numMatriz[self.pos_x,self.pos_y] != 0:
+        while self.numMatrix[self.pos_x,self.pos_y] != 0:
             self.pos_x = int(rd.randint(0,3))
             self.pos_y = int(rd.randint(0,3))
-        self.numMatriz[self.pos_x,self.pos_y] = 1
+        self.numMatrix[self.pos_x,self.pos_y] = 1
         
-    def inicio(self):
+    def start(self):
         if not(self.moves):
-            self.aparece()
+            self.appear()
             self.moves = True
     
-    def direccion(self):
+    def direction(self):
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_LEFT]:
-            self.izquierda = True
+            self.left = True
             self.move()
             
         if keys[pygame.K_RIGHT]:
-            self.derecha = True
+            self.right = True
             self.move()
             
         if keys[pygame.K_UP]:
-            self.arriba = True
+            self.up = True
             self.move()
             
         if keys[pygame.K_DOWN]:
-            self.abajo = True
+            self.down = True
             self.move()
             
-    def move_izquierda(self):
-        for j in range(len(self.numMatriz)):  
+    def move_left(self):
+        for j in range(len(self.numMatrix)):  
             self.c = []
-            for i in list(self.numMatriz[j,:]):
+            for i in list(self.numMatrix[j,:]):
                 if i != 0:
                     self.c.append(i)
             if len(self.c) != 4:
                 self.c = np.concatenate((self.c,np.zeros([4-len(self.c)],float)),axis = 0)
-            self.numMatriz[j,:] = np.array(self.c)
+            self.numMatrix[j,:] = np.array(self.c)
 
-    def move_derecha(self):
-        for j in range(len(self.numMatriz)):  
+    def move_right(self):
+        for j in range(len(self.numMatrix)):  
             self.c = []
-            for i in list(self.numMatriz[j,:]):
+            for i in list(self.numMatrix[j,:]):
                 if i != 0:
                     self.c.append(i)
             if len(self.c) != 4:
                 self.c = np.concatenate((np.zeros([4-len(self.c)],float),self.c),axis = 0)
-            self.numMatriz[j,:] = np.array(self.c)
+            self.numMatrix[j,:] = np.array(self.c)
 
-    def move_arriba(self):
-        for j in range(len(self.numMatriz)):  
+    def move_up(self):
+        for j in range(len(self.numMatrix)):  
             self.c = []
-            for i in list(self.numMatriz[:,j]):
+            for i in list(self.numMatrix[:,j]):
                 if i != 0:
                     self.c.append(i)
             if len(self.c) != 4:
                 self.c = np.concatenate((self.c,np.zeros([4-len(self.c)],float)),axis = 0)
-            self.numMatriz[:,j] = np.array(self.c)
+            self.numMatrix[:,j] = np.array(self.c)
 
-    def move_abajo(self):
-        for j in range(len(self.numMatriz)):  
+    def move_down(self):
+        for j in range(len(self.numMatrix)):  
             self.c = []
-            for i in list(self.numMatriz[:,j]):
+            for i in list(self.numMatrix[:,j]):
                 if i != 0:
                     self.c.append(i)
             if len(self.c) != 4:
                 self.c = np.concatenate((np.zeros([4-len(self.c)],float),self.c),axis = 0)
-            self.numMatriz[:,j] = np.array(self.c)
+            self.numMatrix[:,j] = np.array(self.c)
 
-    def suma_izquierda(self):
-        for i in range(len(self.numMatriz)):  
+    def sum_left(self):
+        for i in range(len(self.numMatrix)):  
             self.c = []
-            for j in range(len(self.numMatriz)-1):
-                if self.numMatriz[i,j] == self.numMatriz[i,j+1] and self.numMatriz[i,j] != 0:
-                    self.numMatriz[i,j] += 1
-                    self.numMatriz[i,j+1] = 0
-            for k in self.numMatriz[i,:]:
+            for j in range(len(self.numMatrix)-1):
+                if self.numMatrix[i,j] == self.numMatrix[i,j+1] and self.numMatrix[i,j] != 0:
+                    self.numMatrix[i,j] += 1
+                    self.numMatrix[i,j+1] = 0
+            for k in self.numMatrix[i,:]:
                 if k != 0:
                     self.c.append(k)
             if len(self.c) != 4:
                 self.c = np.concatenate((self.c,np.zeros([4-len(self.c)],int)),axis = 0)
-            self.numMatriz[i,:] = np.array(self.c)
+            self.numMatrix[i,:] = np.array(self.c)
 
-    def suma_arriba(self):
-        for i in range(len(self.numMatriz)):  
+    def sum_up(self):
+        for i in range(len(self.numMatrix)):  
             self.c = []
-            for j in range(len(self.numMatriz)-1):
-                if self.numMatriz[j,i] == self.numMatriz[j+1,i] and self.numMatriz[j,i] != 0:
-                    self.numMatriz[j,i] += 1
-                    self.numMatriz[j+1,i] = 0
-            for k in self.numMatriz[:,i]:
+            for j in range(len(self.numMatrix)-1):
+                if self.numMatrix[j,i] == self.numMatrix[j+1,i] and self.numMatrix[j,i] != 0:
+                    self.numMatrix[j,i] += 1
+                    self.numMatrix[j+1,i] = 0
+            for k in self.numMatrix[:,i]:
                 if k != 0:
                     self.c.append(k)
             if len(self.c) != 4:
                 self.c = np.concatenate((self.c,np.zeros([4-len(self.c)],int)),axis = 0)
-            self.numMatriz[:,i] = np.array(self.c)
+            self.numMatrix[:,i] = np.array(self.c)
 
-    def suma_abajo(self):
-        for i in range(len(self.numMatriz)):  
+    def sum_down(self):
+        for i in range(len(self.numMatrix)):  
             self.c = []
-            for j in range(self.tam-1):
-                if self.numMatriz[self.tam - (j+1),i] == self.numMatriz[self.tam -(j+2),i] and self.numMatriz[self.tam - (j+1),i] != 0:
-                    self.numMatriz[self.tam - (j+1),i] += 1
-                    self.numMatriz[self.tam - (j+2),i] = 0
-            for k in self.numMatriz[:,i]:
+            for j in range(self.length-1):
+                if self.numMatrix[self.length - (j+1),i] == self.numMatrix[self.length -(j+2),i] and self.numMatrix[self.length - (j+1),i] != 0:
+                    self.numMatrix[self.length - (j+1),i] += 1
+                    self.numMatrix[self.length - (j+2),i] = 0
+            for k in self.numMatrix[:,i]:
                 if k != 0:
                     self.c.append(k)
             if len(self.c) != 4:
                 self.c = np.concatenate((np.zeros([4-len(self.c)],int),self.c),axis = 0)
-            self.numMatriz[:,i] = np.array(self.c)
+            self.numMatrix[:,i] = np.array(self.c)
 
-    def suma_derecha(self):
-        for i in range(len(self.numMatriz)):  
+    def sum_right(self):
+        for i in range(len(self.numMatrix)):  
             self.c = []
-            for j in range(len(self.numMatriz)-1):
-                if self.numMatriz[i,j] == self.numMatriz[i,j+1] and self.numMatriz[i,j] != 0:
-                    self.numMatriz[i,j] += 1
-                    self.numMatriz[i,j+1] = 0
-            for k in self.numMatriz[i,:]:
+            for j in range(len(self.numMatrix)-1):
+                if self.numMatrix[i,j] == self.numMatrix[i,j+1] and self.numMatrix[i,j] != 0:
+                    self.numMatrix[i,j] += 1
+                    self.numMatrix[i,j+1] = 0
+            for k in self.numMatrix[i,:]:
                 if k != 0:
                     self.c.append(k)
             if len(self.c) != 4:
                 self.c = np.concatenate((np.zeros([4-len(self.c)],int),self.c),axis = 0)
-            self.numMatriz[i,:] = np.array(self.c)
+            self.numMatrix[i,:] = np.array(self.c)
     
     def move(self):    
-        self.numMatrizAntes = np.empty(np.shape(self.numMatriz))
-        for i in range(np.shape(self.numMatrizAntes)[0]):
-            for j in range(np.shape(self.numMatrizAntes)[1]):
-                self.numMatrizAntes[i,j] = self.numMatriz[i,j]
+        self.numMatrizBefore = np.empty(np.shape(self.numMatrix))
+        for i in range(np.shape(self.numMatrizBefore)[0]):
+            for j in range(np.shape(self.numMatrizBefore)[1]):
+                self.numMatrizBefore[i,j] = self.numMatrix[i,j]
         
-        if self.izquierda:
-            self.move_izquierda()
-            self.suma_izquierda()
-            self.move_izquierda()
+        if self.left:
+            self.move_left()
+            self.sum_left()
+            self.move_left()
             
-        if self.arriba:
-            self.move_arriba()
-            self.suma_arriba()
-            self.move_arriba()
+        if self.up:
+            self.move_up()
+            self.sum_up()
+            self.move_up()
         
-        if self.derecha:
-            self.move_derecha()
-            self.suma_derecha()
-            self.move_derecha()
+        if self.right:
+            self.move_right()
+            self.sum_right()
+            self.move_right()
             
-        if self.abajo:
-            self.move_abajo()
-            self.suma_abajo()
-            self.move_abajo()
+        if self.down:
+            self.move_down()
+            self.sum_down()
+            self.move_down()
             
-        for i in range(len(self.numMatriz)):
-            for j in range(len(self.numMatriz)):
-                if int(self.numMatriz[i,j]) != int(self.numMatrizAntes[i,j]) and self.cont == 0:
-                    self.aparece()
-                    self.cont += 1
+        for i in range(len(self.numMatrix)):
+            for j in range(len(self.numMatrix)):
+                if int(self.numMatrix[i,j]) != int(self.numMatrizBefore[i,j]) and self.counter == 0:
+                    self.appear()
+                    self.counter += 1
         
-        self.izquierda = False
-        self.arriba = False
-        self.derecha = False
-        self.abajo = False
+        self.left = False
+        self.up = False
+        self.right = False
+        self.down = False
         time.sleep(0.3)
-        self.cont = 0
+        self.counter = 0
         
-    def muerte(self):
-        self.contMuerte = 0
+    def die(self):
+        self.deathCounter = 0
         for i in range(4):
             for j in range(4):
-                if self.numMatriz[i,j] == 0:
-                    self.contMuerte += 1
+                if self.numMatrix[i,j] == 0:
+                    self.deathCounter += 1
         for i in range(3):
-            if self.numMatriz[i,3] == self.numMatriz[i+1,3]:
-                self.contMuerte += 1
-            if self.numMatriz[3,i] == self.numMatriz[3,i+1]:
-                self.contMuerte += 1
+            if self.numMatrix[i,3] == self.numMatrix[i+1,3]:
+                self.deathCounter += 1
+            if self.numMatrix[3,i] == self.numMatrix[3,i+1]:
+                self.deathCounter += 1
             for j in range(3): 
-                if self.numMatriz[i,j] == self.numMatriz[i,j+1]:
-                    self.contMuerte += 1
-                if self.numMatriz[i,j] == self.numMatriz[i+1,j]:
-                    self.contMuerte += 1
+                if self.numMatrix[i,j] == self.numMatrix[i,j+1]:
+                    self.deathCounter += 1
+                if self.numMatrix[i,j] == self.numMatrix[i+1,j]:
+                    self.deathCounter += 1
             
-        if self.contMuerte == 0:
-            #time.sleep(1)
-            self.muerto = True
+        if self.deathCounter == 0:
+            self.death = True
         
     def draw(self,win):
-        self.inicio()
-        self.direccion()
-        self.muerte()
+        self.start()
+        self.direction()
+        self.die()
         self.posx = 23
-        for i in range(len(self.numMatriz)):
+        for i in range(len(self.numMatrix)):
             self.posy = 128
-            for j in range(len(self.numMatriz)):
-                if self.numMatriz[j,i] != 0:
-                    win.blit(Numeros[int(round(self.numMatriz[j,i]))-1],(self.posx,self.posy))
+            for j in range(len(self.numMatrix)):
+                if self.numMatrix[j,i] != 0:
+                    win.blit(Numbers_Images[int(round(self.numMatrix[j,i]))-1],(self.posx,self.posy))
                 self.posy += 100
             self.posx += 100
                 
-matrix = matriz()
+matrix = Matrix()
 
-class restart(object):
+class NewGame(object):
     def draw(self,win):
-        if matrix.muerto:
+        if matrix.death:
             win.blit(GameOver,(-25,0))
         else:
             win.blit(Restart,(-25,0))
-        self.ratonX = pygame.mouse.get_pos()[0]
-        self.ratonY = pygame.mouse.get_pos()[1]
-        if self.ratonX > 100 and self.ratonX < 350:
-            if self.ratonY > 25 and self.ratonY < 100:
-                if matrix.muerto:
+        self.mouseX = pygame.mouse.get_pos()[0]
+        self.mouseY = pygame.mouse.get_pos()[1]
+        if self.mouseX > 100 and self.mouseX < 350:
+            if self.mouseY > 25 and self.mouseY < 100:
+                if matrix.death:
                     win.blit(GameRestart,(-25,0))
                 else:
                     win.blit(Restart_2,(-25,0))
                 if pygame.mouse.get_pressed()[0]:
                     matrix.moves = False
-                    matrix.muerto = False
-                    matrix.numMatriz = np.zeros([4,4],float)
+                    matrix.death = False
+                    matrix.numMatrix = np.zeros([4,4],float)
                     time.sleep(0.3)
                     
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             matrix.moves = False
-            matrix.numMatriz = np.zeros([4,4],float)
+            matrix.numMatrix = np.zeros([4,4],float)
             time.sleep(0.3)
             
-nuevaPartida = restart()
+newGame = NewGame()
 
 def redrawWinGame():
     win.blit(Bg,(0,0))
-    nuevaPartida.draw(win)
-    mallas.draw(win)
+    newGame.draw(win)
+    mesh.draw(win)
     matrix.draw(win)
     pygame.display.update()
 
